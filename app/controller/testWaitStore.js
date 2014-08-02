eda8d87d343bb05f390ae9262b621f4da2258c37
Garnel Mao <maogm12@gmail.com>
2014-08-02 18:36:07 +0800

    wait page ok

--- app/controller/testWaitStore.js
+++ app/controller/testWaitStore.js
@@ -108,7 +108,7 @@ App.waitStore = sumeru.controller.create(function(env, session){
             session.bind('storeinfo_container', {
                 data:array_after_filter,
             });
-            doRender('testWaitStore', ['push', 'left']);
+            doRender('store', ['push', 'left']);
             loghu('render end');
         } catch(e){
             loghu(e.name+":"+e.message);
