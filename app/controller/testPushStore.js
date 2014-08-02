3c91117a117d242291609381ed5f66bcdc8fce9d
thunder <wikinewsm@gmail.com>
2014-08-02 21:06:37 +0800

    add feature:get a store by id

--- app/controller/testPushStore.js
+++ app/controller/testPushStore.js
@@ -15,9 +15,6 @@ App.pushStore = sumeru.controller.create(function(env, session){
     var getStores = function(){
         try{
             session.Store = env.subscribe('pub-pushStoreAll', function(storeCollection){
-                session.bind('storeinfo_container', {
-                    data:storeCollection.find()
-                });
             });
         } catch(e){
             loghu(e.name+":"+e.message);
@@ -39,8 +36,24 @@ App.pushStore = sumeru.controller.create(function(env, session){
         return [getStores];
     };
 
+    var getIdStore = function(stores, id){
+        var retArray = [];
+        var len = stores.length;
+        for(var i=0 ; i<len ; i++){
+            if(stores.get(i).id == id){
+                return stores.get(i);
+            };
+        }
+    };
+
     env.onrender = function(doRender){
         try{
+            var arraystore = session.Store.find();
+            var thestore = getIdStore(arraystore, parseInt(session.get("id")));
+            session.bind('storeinfo_container', {
+                //data:storeCollection.find({"id":parseInt(session.get("id"))})
+                data:thestore
+            });
             doRender('testStore', ['push', 'left']);
             loghu('render end');
         } catch(e){
