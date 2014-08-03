6e82c8f5faef2fab5c1332ecebe4009d91c3c9b3
thunder <wikinewsm@gmail.com>
2014-08-03 10:37:00 +0800

    全新编造的数据

--- app/personal_hu/addData/addPushStoreData.js
+++ app/personal_hu/addData/addPushStoreData.js
@@ -1,11 +1,11 @@
 db.pushStore.drop();
 db.pushStore.save({
-    name:'KFC',
+    name:'小首饰',
     id:1,
     coordinate:[116.4452, 39.96097],
-    description:"肯德基，啃得起",
+    description:"精美饰品，买个给女朋友吧",
     cost_time:25,
-    picture:'http://www.78.cn/kb/uploadfile/userfiles/20130725/20130725100753_24292.jpg'
+    picture:'http://img5.imgtn.bdimg.com/it/u=2700335865,1551599504&fm=21&gp=0.jpg'
 });
 
 db.pushStore.save({
@@ -14,7 +14,7 @@ db.pushStore.save({
     coordinate:[116.4452, 39.96097],
     description:"喝杯咖啡吧，先森",
     cost_time:30,
-    picture:'http://cdn.duitang.com/uploads/item/201212/22/20121222234031_rsQn5.thumb.600_0.jpeg',
+    picture:'http://182.92.98.37/pics/starbucks.jpeg',
 });
 
 
@@ -23,7 +23,7 @@ db.pushStore.save({
     id:'3',
     coordinate:[116.4452, 39.96097],
     description:"优雅淑女",
-    cost_time:30,
+    cost_time:45,
     picture:'http://fashion.365jia.cn/uploads/news/users/images/editor152/image/2.jpg',
 });
 
@@ -32,7 +32,7 @@ db.pushStore.save({
     id:'4',
     coordinate:[116.4452, 39.96097],
     description:"喜欢就会放肆，而爱就是克制",
-    cost_time:30,
+    cost_time:130,
     picture:'http://ico.ooopic.com/ajax/iconpng/?id=161489.png',
 });
 
@@ -41,6 +41,6 @@ db.pushStore.save({
     id:'5',
     coordinate:[116.4452, 39.96097],
     description:"你买不起的",
-    cost_time:30,
+    cost_time:40,
     picture:'http://img2.duitang.com/uploads/item/201207/06/20120706151814_w5A3H.thumb.600_0.jpeg',
 });
